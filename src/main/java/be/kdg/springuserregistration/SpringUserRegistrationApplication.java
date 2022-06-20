1c052f7c6c47a74b1b2b4cfeaaf2f663274e8f09
Nicolas Bouquiaux <nicolas.bouquiaux@student.kdg.be>
2022-06-20 17:42:33 +0200

    Initial commit

---
+++ src/main/java/be/kdg/springuserregistration/SpringUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.springuserregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/kdg/springuserregistration/SpringUserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.springuserregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
